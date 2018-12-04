686a95c536b0e383ec7e2a4d55720c5267d59ae5
Thibault COLLARD <tcollard@e2r10p11.42.fr>
2018-12-04 19:02:23 +0100

    save for today

--- srcs/parser/parser.c
+++ srcs/parser/parser.c
@@ -6,7 +6,7 @@
 /*   By: tcollard <tcollard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/01 11:48:48 by tcollard          #+#    #+#             */
-/*   Updated: 2018/12/04 16:25:37 by tcollard         ###   ########.fr       */
+/*   Updated: 2018/12/04 17:13:29 by tcollard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -71,7 +71,7 @@ static void		sort_ast(t_ast *lst, t_ast **sort)
 		{
 			tmp->left = *sort;
 			(*sort)->back = tmp;
-			*sort = tmp;	
+			*sort = tmp;
 		}
 		else if (tmp->type != CMD)
 		{
@@ -91,58 +91,135 @@ static void		sort_ast(t_ast *lst, t_ast **sort)
 	}
 }
 
-static void		read_sort(t_ast *sort)
+static void			read_sort(t_ast *sort)
 {
 	t_ast	*tmp;
-	t_ast	*save;
 	int		i;
 
 	tmp = sort;
-	save = NULL;
-	i = 0;
 	while (tmp->left)
-		tmp = tmp->left;
-	while (tmp)
 	{
-		(tmp->print == 0) ? ft_printf("\ntype = %d\n", tmp->type) : 0;
+		ft_printf("\ntype= %d\n", tmp->type);
 		i = 0;
-		while (tmp->input[i] && tmp->print == 0)
+		while (tmp->input[i])
 		{
 			ft_printf("input[%d]: %s\n", i, tmp->input[i]);
 			i += 1;
 		}
 		tmp->print = 1;
-		if (tmp->right && tmp->right->print == 0)
-		{
-			if (tmp->right->type != CMD)
-			{
-				tmp = tmp->right;
-				while (tmp->left)
-					tmp = tmp->left;
-				(tmp->print == 0) ? ft_printf("\ntype = %d\n", tmp->type) : 0;
-				i = 0;
-				while (tmp->input[i] && tmp->print == 0)
-				{
-					ft_printf("input[%d]: %s\n", i, tmp->input[i]);
-					i += 1;
-				}
-				tmp->print = 1;
-			}
-			else
-			{
-				(tmp->right->print == 0) ? ft_printf("\ntype = %d\n", tmp->right->type) : 0;
-				i = 0;
-				while (tmp->right->input[i] && tmp->right->print == 0)
-				{
-					ft_printf("input[%d]: %s\n", i, tmp->right->input[i]);
-					i += 1;
-				}
-				tmp->right->print = 1;
-			}
-		}
-		tmp = tmp->back;
+		tmp = tmp->left;
 	}
+	ft_printf("\ntype= %d\n", tmp->type);
+	i = 0;
+	while (tmp->input[i])
+	{
+		ft_printf("input[%d]: %s\n", i, tmp->input[i]);
+		i += 1;
+	}
+	tmp->print = 1;
+	// while (tmp)
+	// {
+ 	// 	(tmp->print == 0) ? ft_printf("\ntype = %d\n", tmp->type) : 0;
+ 	// 	i = 0;
+ 	// 	while (tmp->input[i] && tmp->print == 0)
+ 	// 	{
+ 	// 		ft_printf("input[%d]: %s\n", i, tmp->input[i]);
+ 	// 		i += 1;
+ 	// 	}
+	// 	tmp->print = 1;
+ 	// 	if (tmp->right && tmp->right->print == 0)
+ 	// 	{
+ 	// 		if (tmp->right->type != CMD)
+ 	// 		{
+ 	// 			tmp = tmp->right;
+	// 			while (tmp->left)
+	// 			{
+	// 				ft_printf("\ntype= %d\n", tmp->type);
+	// 				i = 0;
+	// 				while (tmp->input[i])
+	// 				{
+	// 					ft_printf("input[%d]: %s\n", i, tmp->input[i]);
+	// 					i += 1;
+	// 				}
+	// 				tmp->print = 1;
+	// 				tmp = tmp->left;
+	// 			}
+ 	// 			(tmp->print == 0) ? ft_printf("\ntype = %d\n", tmp->type) : 0;
+ 	// 			i = 0;
+ 	// 			while (tmp->input[i] && tmp->print == 0)
+ 	// 			{
+ 	// 				ft_printf("input[%d]: %s\n", i, tmp->input[i]);
+ 	// 				i += 1;
+ 	// 			}
+ 	// 			tmp->print = 1;
+ 	// 		}
+ 	// 		else
+ 	// 		{
+ 	// 			(tmp->right->print == 0) ? ft_printf("\ntype = %d\n", tmp->right->type) : 0;
+ 	// 			i = 0;
+ 	// 			while (tmp->right->input[i] && tmp->right->print == 0)
+ 	// 			{
+ 	// 				ft_printf("input[%d]: %s\n", i, tmp->right->input[i]);
+ 	// 				i += 1;
+ 	// 			}
+ 	// 			tmp->right->print = 1;
+ 	// 		}
+	// 	}
+	// }
 }
+
+// static void		read_sort(t_ast *sort)
+// {
+// 	t_ast	*tmp;
+// 	t_ast	*save;
+// 	int		i;
+//
+// 	tmp = sort;
+// 	save = NULL;
+// 	i = 0;
+// 	while (tmp->left)
+// 		tmp = tmp->left;
+// 	while (tmp)
+// 	{
+// 		(tmp->print == 0) ? ft_printf("\ntype = %d\n", tmp->type) : 0;
+// 		i = 0;
+// 		while (tmp->input[i] && tmp->print == 0)
+// 		{
+// 			ft_printf("input[%d]: %s\n", i, tmp->input[i]);
+// 			i += 1;
+// 		}
+// 		tmp->print = 1;
+// 		if (tmp->right && tmp->right->print == 0)
+// 		{
+// 			if (tmp->right->type != CMD)
+// 			{
+// 				tmp = tmp->right;
+// 				while (tmp->left)
+// 					tmp = tmp->left;
+// 				(tmp->print == 0) ? ft_printf("\ntype = %d\n", tmp->type) : 0;
+// 				i = 0;
+// 				while (tmp->input[i] && tmp->print == 0)
+// 				{
+// 					ft_printf("input[%d]: %s\n", i, tmp->input[i]);
+// 					i += 1;
+// 				}
+// 				tmp->print = 1;
+// 			}
+// 			else
+// 			{
+// 				(tmp->right->print == 0) ? ft_printf("\ntype = %d\n", tmp->right->type) : 0;
+// 				i = 0;
+// 				while (tmp->right->input[i] && tmp->right->print == 0)
+// 				{
+// 					ft_printf("input[%d]: %s\n", i, tmp->right->input[i]);
+// 					i += 1;
+// 				}
+// 				tmp->right->print = 1;
+// 			}
+// 		}
+// 		tmp = tmp->back;
+// 	}
+// }
 
 void			parser(char **input, t_ast *lst, t_env *lst_env)
 {
