025eab3ef777eba7edc19b3fe6ff846448db69d0
Thibault COLLARD <tcollard@e2r10p9.42.fr>
2018-12-13 14:23:14 +0100

    Save merge of Tibo branch

--- srcs/builtins/dispatch_builtins.c
+++ srcs/builtins/dispatch_builtins.c
@@ -6,20 +6,11 @@
 /*   By: tcollard <tcollard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/16 19:32:45 by tcollard          #+#    #+#             */
-/*   Updated: 2018/10/19 11:03:00 by tcollard         ###   ########.fr       */
+/*   Updated: 2018/12/13 14:21:35 by tcollard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../includes/ft_21sh.h"
-
-/*
-** LIST BUILTINS:
-**		- CD
-**		- ECHO
-**		- SETENV
-**		- UNSETENV
-**		- ENV
-*/
 
 void	builtins_dispatch(t_ast *elem, t_list **env)
 {
@@ -27,21 +18,11 @@ void	builtins_dispatch(t_ast *elem, t_list **env)
 	int			i;
 
 	(void)env;
-	// ft_printf("Value = %s\n", elem->value);
-	ft_printf("\n\nENV:\n");
-	// i = 0;
-	// while (tab_env[i])
-	// {
-	// 	ft_printf("%s\n", tab_env[i]);
-	// 	i += 1;
-	// }
 	i = 0;
 	while (i < 5)
 	{
 		if (ft_strcmp(elem->value, lst_built[i]) == 0)
 			break ;
-		// ft_printf("BUILTINS[%d]: %s\n\n\n", i, lst_built[i]);
 		i += 1;
 	}
-	// (i == 5) ? ft_printf("Execve\n") : ft_printf("builtins[%d]: %s\n", i, lst_built[i]);
 }
