0d2f7b92c17d9940ea5692ffc358ccf818e8085e
Thibault COLLARD <tcollard@e2r10p9.42.fr>
2018-12-13 19:19:25 +0100

    save for today

--- srcs/error/exec_error.c
+++ srcs/error/exec_error.c
@@ -6,7 +6,7 @@
 /*   By: tcollard <tcollard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/07 12:10:58 by tcollard          #+#    #+#             */
-/*   Updated: 2018/12/12 15:31:26 by tcollard         ###   ########.fr       */
+/*   Updated: 2018/12/13 19:19:21 by tcollard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,10 +14,10 @@
 
 int	exec_error(int err, char *files, t_alloc **alloc)
 {
+	(void)alloc;
 	(err == -1) ? write(2, "21sh: commande not found: ", 26) : 0;
 	if (err == -1)
 		write(2, files, ft_strlen(files));
 	write(2, "\n", 1);
-	del_alloc(alloc);
 	return (err);
 }
