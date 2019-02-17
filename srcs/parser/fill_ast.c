534dc7106a9d1f3256cb66f4b3321df1c5ba329a
tcollard <tcollard@student.42.fr>
2019-02-17 20:47:31 +0100

    update parser and fill ast for issue 23

--- srcs/parser/fill_ast.c
+++ srcs/parser/fill_ast.c
@@ -6,7 +6,7 @@
 /*   By: tcollard <tcollard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/23 19:15:26 by tcollard          #+#    #+#             */
-/*   Updated: 2019/02/07 13:10:13 by tcollard         ###   ########.fr       */
+/*   Updated: 2019/02/17 20:44:46 by tcollard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,13 +33,15 @@ static void	fill_input(char **s, int end, int start, t_ast *elem)
 
 static void	fill_input_redir(char *s, t_ast *elem)
 {
-	int	i;
-	int	save;
-	int	wd;
+	int		i;
+	int		save;
+	int		wd;
+	char	c;
 
 	i = 0;
 	save = i;
 	wd = 0;
+	c = '\0';
 	while (s[i])
 		if (ft_isdigit(s[i]) == 1)
 		{
@@ -49,12 +51,28 @@ static void	fill_input_redir(char *s, t_ast *elem)
 			wd += 1;
 			save = i;
 		}
-		else
+		else if (ft_isoperator(s[i])  == 1)
 		{
 			while (ft_isoperator(s[i]) == 1)
 				i += 1;
 			i += (s[i] == '-') ? 1 : 0;
 			elem->input[wd] = ft_strsub(s, save, i - save);
+			wd += 1;
+			save = i;
+		}
+		else
+		{
+			while (ft_isoperator(s[i]) == 0)
+			{
+				if (ft_isquote(s[i]) == 1)
+				{
+					c = s[i];
+					while (s[i] != c)
+						i += 1;
+				}
+				i += 1;
+			}
+			elem->input[wd] = (c == '\0') ? ft_strsub(s, save, i - save) :ft_strsub(s, save,  i - save + 3);
 			wd += 1;
 			save = i;
 		}
@@ -64,9 +82,11 @@ static void	split_redir(char *s, t_ast *elem)
 {
 	int		wd;
 	int		i;
+	char	c;
 
 	wd = 0;
 	i = 0;
+	c = '\0';
 	while (s[i])
 		if (ft_isdigit(s[i]) == 1)
 		{
@@ -74,12 +94,26 @@ static void	split_redir(char *s, t_ast *elem)
 			while (ft_isdigit(s[i]) == 1)
 				i += 1;
 		}
-		else
+		else if (ft_isoperator(s[i]) == 1)
 		{
 			wd += 1;
 			while (ft_isoperator(s[i]) == 1)
 				i += 1;
 			i += (s[i] == '-') ? 1 : 0;
+		}
+		else
+		{
+			wd += 1;
+			while (ft_isoperator(s[i]) == 0)
+			{
+				if (ft_isquote(s[i]) == 1)
+				{
+					c = s[i];
+					while (s[i] != c)
+						i += 1;
+				}
+				i += 1;
+			}
 		}
 	if (!(elem->input = (char**)malloc(sizeof(char*) * (wd + 1))))
 		return ;
@@ -120,6 +154,7 @@ void		fill_ast(char **s, t_ast **lst)
 	int		i;
 	int		x;
 	int		save;
+	char	c;
 
 	i = -1;
 	save = 0;
@@ -128,6 +163,7 @@ void		fill_ast(char **s, t_ast **lst)
 	{
 		x = -1;
 		while (s[i][++x])
+		{
 			if (ft_isoperator(s[i][0]) == 1 || (ft_isoperator(s[i][x]) == 1
 			&& ft_isdigit(s[i][x - 1]) == 1))
 			{
@@ -138,6 +174,16 @@ void		fill_ast(char **s, t_ast **lst)
 				save = i + 1;
 				break ;
 			}
+			else if (ft_isquote(s[i][x]) == 1)
+			{
+				c = s[i][x];
+				x += 1;
+				while (s[i][x] != c)
+					x += 1;
+				x += 1;
+				break ;
+			}
+		}
 	}
 	(new->type != NO_TYPE && i != save) ? new = add_new_elem(lst) : 0;
 	(i != save) ? fill_input(s, i, save, new) : 0;
